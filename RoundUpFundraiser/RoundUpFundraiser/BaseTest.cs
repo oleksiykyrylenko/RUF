e62152018f3329392842245a939b8517f85960b8
Oleksiy Kyrylenko <alex228015@gmail.com>
2017-12-22 15:35:48 -0500

    Created SetUP and TearDown Methods

--- RoundUpFundraiser/RoundUpFundraiser/BaseTest.cs
+++ RoundUpFundraiser/RoundUpFundraiser/BaseTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,18 @@ namespace RoundUpFundraiser
 {
     public class BaseTest
     {
+        protected IWebDriver driver;
+
+        [SetUp]  
+        public void SetUP()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
     }
 }
